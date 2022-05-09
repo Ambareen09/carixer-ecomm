2169ed6e3af50e36fa2acf6d610353406c8bac0a
Ambareen09 <ambubushra@gmail.com>
2022-05-09 07:50:31 +0000

    adminpanel app ADD

--- carixerecomm/carixerecomm/urls.py
+++ carixerecomm/carixerecomm/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin", admin.site.urls),
+    path("/adminpanel", include("adminpanel.urls")),
     path("", include("website.urls")),
 ]

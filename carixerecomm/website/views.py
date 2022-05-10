575c8e006e2091222c1124d904c307f119869091
Ambareen09 <ambubushra@gmail.com>
2022-05-10 21:00:23 +0000

    devcontainer update

--- carixerecomm/website/views.py
+++ carixerecomm/website/views.py
@@ -16,9 +16,9 @@ from .models import Product, OrderDetail, About, Waterless, DeliveryCheckpoint
 
 def register(request):
     data = request.POST
-    username = data['username']
-    password = data['password']
-    email = data['email']
+    username = data["username"]
+    password = data["password"]
+    email = data["email"]
 
     if User.objects.filter(username=username).exists():
         return index(request)
@@ -26,178 +26,205 @@ def register(request):
     if User.objects.filter(email=email).exists():
         return index(request)
 
-    user = User.objects.create_user(
-        username=username, email=email, password=password)
+    user = User.objects.create_user(username=username, email=email, password=password)
     return index(request)
 
 
 class UserLogin(View):
     def post(self, request):
         data = request.POST
-        username = data['username']
-        password = data['password']
+        username = data["username"]
+        password = data["password"]
         user = authenticate(username=username, password=password)
         if user:
             if user.is_active:
                 login(request, user)
-                messages.success(request, 'login succesful')
+                messages.success(request, "login succesful")
             else:
-                messages.error(request, 'inactive account')
+                messages.error(request, "inactive account")
         else:
-            messages.error(request, 'invalid credentials')
-        return HttpResponseRedirect(request.headers['Referer'])
+            messages.error(request, "invalid credentials")
+        return HttpResponseRedirect(request.headers["Referer"])
 
 
 def cartItems(request):
-    cart = list(OrderDetail.objects.filter(
-        user__id=request.user.id, status="INCART").values())
+    cart = list(
+        OrderDetail.objects.filter(user__id=request.user.id, status="INCART").values()
+    )
     for o in cart:
-        p = Product.objects.get(id=o['product_id'])
-        o['title'] = p.title
-        o['image'] = str(p.image)
-        o['price'] = p.price
-        o['totalPrice'] = o['quantity']*p.price
-        o['save'] = o['totalPrice'] + 0.5*o['totalPrice']
-        o['percentSave'] = 50
-        temp = (o['status'])
-        o['status_color'] = ""
+        p = Product.objects.get(id=o["product_id"])
+        o["title"] = p.title
+        o["image"] = str(p.image)
+        o["price"] = p.price
+        o["totalPrice"] = o["quantity"] * p.price
+        o["save"] = o["totalPrice"] + 0.5 * o["totalPrice"]
+        o["percentSave"] = 50
+        temp = o["status"]
+        o["status_color"] = ""
         if temp == "DELIVERED":
-            o['status_color'] = "delivery"
+            o["status_color"] = "delivery"
         elif temp == "ORDERED":
-            o['status_color'] = "order"
+            o["status_color"] = "order"
         elif temp == "CANCELLED":
-            o['status_color'] = "cancel"
+            o["status_color"] = "cancel"
         else:
-            o['status_color'] = "cancel"
+            o["status_color"] = "cancel"
     return cart
 
 
 def cart(cartItems):
-    cartList = {'grandTotal': 0}
+    cartList = {"grandTotal": 0}
     print(cartItems)
     for c in cartItems:
-        cartList['grandTotal'] += c['totalPrice']
+        cartList["grandTotal"] += c["totalPrice"]
     return cartList
 
 
 def index(request):
     about = list(About.objects.values())
     cart = cartItems(request)
-    products = ProductSerializer().serialize(Product.objects.all(), fields=[
-        'id',  'title', 'price', 'image', 'featured', 'short_description', 'long_description', 'reviews'])
-    products = [p['fields'] for p in json.loads(products)]
+    products = ProductSerializer().serialize(
+        Product.objects.all(),
+        fields=[
+            "id",
+            "title",
+            "price",
+            "image",
+            "featured",
+            "short_description",
+            "long_description",
+            "reviews",
+        ],
+    )
+    products = [p["fields"] for p in json.loads(products)]
     for p in products:
-        reviews = p['reviews']
+        reviews = p["reviews"]
         count = len(reviews)
-        rates = [r['rate'] for r in reviews]
-        p['rating'] = {'count': count, 'rate': sum(rates)/max(1, count)}
-    return render(request, 'index.html', {
-        "products": products, "about": about, "cart": cart
-    })
+        rates = [r["rate"] for r in reviews]
+        p["rating"] = {"count": count, "rate": sum(rates) / max(1, count)}
+    return render(
+        request, "index.html", {"products": products, "about": about, "cart": cart}
+    )
 
 
 def about(request):
     about = list(About.objects.values())
     cart = cartItems(request)
-    return render(request, 'about.html', {'about': about, "cart": cart})
+    return render(request, "about.html", {"about": about, "cart": cart})
 
 
 def productlist(request):
     query_products = Product.objects.exclude(status="DRAFT")
-    if 'search' in request.GET:
-        query_products = query_products.filter(
-            title__icontains=request.GET['search'])
-        if 'sort' in request.GET:
+    if "search" in request.GET:
+        query_products = query_products.filter(title__icontains=request.GET["search"])
+        if "sort" in request.GET:
             query_products = query_products.order_by(
-                ('-' if request.GET['sort'] == 'desc' else '')+'price')
-    elif 'sort' in request.GET:
+                ("-" if request.GET["sort"] == "desc" else "") + "price"
+            )
+    elif "sort" in request.GET:
         query_products = query_products.order_by(
-            ('-' if request.GET['sort'] == 'desc' else '')+'price')
-    products = ProductSerializer().serialize(query_products, fields=[
-        'id',   'title', 'price', 'image', 'featured', 'short_description', 'long_description', 'reviews'])
-    products = [p['fields'] for p in json.loads(products)]
+            ("-" if request.GET["sort"] == "desc" else "") + "price"
+        )
+    products = ProductSerializer().serialize(
+        query_products,
+        fields=[
+            "id",
+            "title",
+            "price",
+            "image",
+            "featured",
+            "short_description",
+            "long_description",
+            "reviews",
+        ],
+    )
+    products = [p["fields"] for p in json.loads(products)]
     cart = cartItems(request)
     for p in products:
-        reviews = p['reviews']
+        reviews = p["reviews"]
         count = len(reviews)
-        rates = [r['rate'] for r in reviews]
-        p['rating'] = {'count': count, 'rate': sum(rates)/max(1, count)}
+        rates = [r["rate"] for r in reviews]
+        p["rating"] = {"count": count, "rate": sum(rates) / max(1, count)}
 
-    return render(request, 'productlist.html', {
-        "products": products, "cart": cart
-    })
+    return render(request, "productlist.html", {"products": products, "cart": cart})
 
 
 def waterless(request):
     waterless = list(Waterless.objects.values())
     cart = cartItems(request)
     print(waterless)
-    return render(request, 'waterless.html', {"waterless": waterless, "cart": cart})
+    return render(request, "waterless.html", {"waterless": waterless, "cart": cart})
 
 
 def orders(request):
     orders = list(OrderDetail.objects.exclude(status="INCART").values())
     cart = cartItems(request)
     for o in orders:
-        product = list(Product.objects.filter(id=o['product_id']).values())
+        product = list(Product.objects.filter(id=o["product_id"]).values())
         for p in product:
-            o['title'] = p['title']
-            o['size'] = p['size']
-            o['totalPrice'] = o['quantity']*p['price']
-            o['save'] = o['totalPrice'] + 0.5*o['totalPrice']
-        o['percentSave'] = 50
-        temp = (o['status'])
-        o['status_color'] = ""
+            o["title"] = p["title"]
+            o["size"] = p["size"]
+            o["totalPrice"] = o["quantity"] * p["price"]
+            o["save"] = o["totalPrice"] + 0.5 * o["totalPrice"]
+        o["percentSave"] = 50
+        temp = o["status"]
+        o["status_color"] = ""
         if temp == "DELIVERED":
-            o['status_color'] = "delivery"
+            o["status_color"] = "delivery"
         elif temp == "ORDERED":
-            o['status_color'] = "order"
+            o["status_color"] = "order"
         elif temp == "CANCELLED":
-            o['status_color'] = "cancel"
+            o["status_color"] = "cancel"
         else:
-            o['status_color'] = "order"
-    return render(request, 'orders.html', {"orders": orders, "cart": cart})
+            o["status_color"] = "order"
+    return render(request, "orders.html", {"orders": orders, "cart": cart})
 
 
 def ordersdetail(request, id):
     orders = list(OrderDetail.objects.filter(id=id).values())
     cart = cartItems(request)
     for o in orders:
-        product = list(Product.objects.filter(id=o['product_id']).values())
+        product = list(Product.objects.filter(id=o["product_id"]).values())
         for p in product:
-            o['title'] = p['title']
-            o['size'] = p['size']
-            o['totalPrice'] = o['quantity']*p['price']
-            o['save'] = o['totalPrice'] + 0.5*o['totalPrice']
-        o['percentSave'] = 50
-        temp = (o['status'])
-        o['status_color'] = ""
+            o["title"] = p["title"]
+            o["size"] = p["size"]
+            o["totalPrice"] = o["quantity"] * p["price"]
+            o["save"] = o["totalPrice"] + 0.5 * o["totalPrice"]
+        o["percentSave"] = 50
+        temp = o["status"]
+        o["status_color"] = ""
         if temp == "DELIVERED":
-            o['status_color'] = "delivery"
+            o["status_color"] = "delivery"
         elif temp == "ORDERED":
-            o['status_color'] = "order"
+            o["status_color"] = "order"
         elif temp == "CANCELLED":
-            o['status_color'] = "cancel"
+            o["status_color"] = "cancel"
         else:
-            o['status_color'] = "cancel"
-        o['checkpoints'] = [model_to_dict(c) for c in DeliveryCheckpoint.objects.filter(
-            order__id=id).order_by("transit_index")]
-    return render(request, 'orderdetail.html', {"orders": orders, "cart": cart})
+            o["status_color"] = "cancel"
+        o["checkpoints"] = [
+            model_to_dict(c)
+            for c in DeliveryCheckpoint.objects.filter(order__id=id).order_by(
+                "transit_index"
+            )
+        ]
+    return render(request, "orderdetail.html", {"orders": orders, "cart": cart})
 
 
 def checkout(request):
-    if request.method == 'POST':
-        country = request.POST.get('country')
-        first_name = request.POST.get('firstname')
-        last_name = request.POST.get('lastname')
-        address = request.POST.get('address')
-        apartment = request.POST.get('apartment')
-        postal_code = request.POST.get('postalcode')
-        city = request.POST.get('city')
-        phone_number = request.POST.get('phonenumber')
-        shipping_charge = request.POST.get('flexRadioDefault')
+    if request.method == "POST":
+        country = request.POST.get("country")
+        first_name = request.POST.get("firstname")
+        last_name = request.POST.get("lastname")
+        address = request.POST.get("address")
+        apartment = request.POST.get("apartment")
+        postal_code = request.POST.get("postalcode")
+        city = request.POST.get("city")
+        phone_number = request.POST.get("phonenumber")
+        shipping_charge = request.POST.get("flexRadioDefault")
 
-        for odo in OrderDetail.objects.filter(user__id=request.user.id, status="INCART"):
+        for odo in OrderDetail.objects.filter(
+            user__id=request.user.id, status="INCART"
+        ):
             odo.country = country
             odo.first_name = first_name
             odo.last_name = last_name
@@ -209,65 +236,97 @@ def checkout(request):
             odo.shipping_charge = shipping_charge
             odo.status = "ORDERED"
             odo.save()
-        return HttpResponseRedirect('/productlist')
+        return HttpResponseRedirect("/productlist")
 
-    if request.method == 'GET':
+    if request.method == "GET":
         cartItem = cartItems(request)
         carts = cart(cartItem)
-        return render(request, 'checkout.html', {"cartItems": cartItem, "cart": carts})
+        return render(request, "checkout.html", {"cartItems": cartItem, "cart": carts})
 
 
 def productdetail(request, id):
     cart = cartItems(request)
     product = Product.objects.get(id=id)
-    product = ProductSerializer().serialize(Product.objects.filter(title=product.title), fields=[
-        'id', 'title', 'size', 'price', 'image', 'featured', 'short_description', 'long_description', 'reviews'])
-    suggestions = ProductSerializer().serialize(Product.objects.all(), fields=[
-        'id',   'title', 'price', 'image', 'featured', 'short_description', 'long_description', 'reviews'])
-    product = [p['fields'] for p in json.loads(product)]
-    suggestions = [p['fields'] for p in json.loads(suggestions)]
+    product = ProductSerializer().serialize(
+        Product.objects.filter(title=product.title),
+        fields=[
+            "id",
+            "title",
+            "size",
+            "price",
+            "image",
+            "featured",
+            "short_description",
+            "long_description",
+            "reviews",
+        ],
+    )
+    suggestions = ProductSerializer().serialize(
+        Product.objects.all(),
+        fields=[
+            "id",
+            "title",
+            "price",
+            "image",
+            "featured",
+            "short_description",
+            "long_description",
+            "reviews",
+        ],
+    )
+    product = [p["fields"] for p in json.loads(product)]
+    suggestions = [p["fields"] for p in json.loads(suggestions)]
     for p in product:
-        reviews = p['reviews']
+        reviews = p["reviews"]
         count = len(reviews)
-        rates = [r['rate'] for r in reviews]
-        p['rating'] = {'count': count, 'rate': sum(rates)/max(1, count)}
+        rates = [r["rate"] for r in reviews]
+        p["rating"] = {"count": count, "rate": sum(rates) / max(1, count)}
 
     for p in suggestions:
-        reviews = p['reviews']
+        reviews = p["reviews"]
         count = len(reviews)
-        rates = [r['rate'] for r in reviews]
-        p['rating'] = {'count': count, 'rate': sum(rates)/max(1, count)}
+        rates = [r["rate"] for r in reviews]
+        p["rating"] = {"count": count, "rate": sum(rates) / max(1, count)}
 
-    sizes = [{'id': p['id'], 'price':p['price'], 'size': p['size']}
-             for p in product]
+    sizes = [{"id": p["id"], "price": p["price"], "size": p["size"]} for p in product]
     print(sizes)
-    return render(request, 'productdetail.html', {
-        "p": product[0], "sizes": sizes, "suggestions": suggestions, "cart": cart
-    })
+    return render(
+        request,
+        "productdetail.html",
+        {"p": product[0], "sizes": sizes, "suggestions": suggestions, "cart": cart},
+    )
 
 
 class cartView(View):
     def put(self, request, id_):
-        data = json.loads(request.body.decode('utf-8'))
-        odo = OrderDetail.objects.get(
-            id=id_, user__id=request.user.id, status="INCART")
-        if data['quantity'] > 0:
-            odo.quantity = data['quantity']
+        data = json.loads(request.body.decode("utf-8"))
+        odo = OrderDetail.objects.get(id=id_, user__id=request.user.id, status="INCART")
+        if data["quantity"] > 0:
+            odo.quantity = data["quantity"]
             odo.save()
         else:
             odo.delete()
-        return HttpResponse({'msg': 'successful'})
+        return HttpResponse({"msg": "successful"})
 
     def post(self, request, id_):
-        data = json.loads(request.body.decode('utf-8'))
+        data = json.loads(request.body.decode("utf-8"))
         from datetime import date
-        if OrderDetail.objects.filter(product__id=id_, user__id=request.user.id, status="INCART").exists():
+
+        if OrderDetail.objects.filter(
+            product__id=id_, user__id=request.user.id, status="INCART"
+        ).exists():
             odo = OrderDetail.objects.get(
-                product__id=id_, user__id=request.user.id, status="INCART")
-            odo.quantity += data['quantity']
+                product__id=id_, user__id=request.user.id, status="INCART"
+            )
+            odo.quantity += data["quantity"]
             odo.save()
         else:
-            odo = OrderDetail(user=request.user, status="INCART", date=date.today(),
-                              product=Product.objects.get(id=id_), quantity=data['quantity'])
+            odo = OrderDetail(
+                user=request.user,
+                status="INCART",
+                date=date.today(),
+                product=Product.objects.get(id=id_),
+                quantity=data["quantity"],
+            )
             odo.save()
-        return HttpResponse({'msg': 'successful'})
+        return HttpResponse({"msg": "successful"})

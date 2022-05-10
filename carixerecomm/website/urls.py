800ce1c51486fa6218b848601d86936d89a3a623
Ambareen09 <ambubushra@gmail.com>
2022-05-10 21:00:39 +0000

    devcontainer update

--- carixerecomm/website/urls.py
+++ carixerecomm/website/urls.py
@@ -5,18 +5,15 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    
     path("register", views.register, name="signup"),
     path("login", views.UserLogin.as_view(), name="signin"),
     path("logout", LogoutView.as_view(), name="signout"),
-    
-    path('about', views.about, name='about'),
-    path('productlist', views.productlist, name='product-list'),
-    path('waterless', views.waterless, name='waterless'),
-    path('orders', views.orders, name='orders'),
-    path('orderdetail/<int:id>', views.ordersdetail, name='order-detail'),
-    path('checkout', views.checkout, name='checkout'),
-    path('productdetail/<int:id>', views.productdetail, name='product-detail'),
-
-    path('cart/<int:id_>', views.cartView.as_view(), name='cart-functions')
+    path("about", views.about, name="about"),
+    path("productlist", views.productlist, name="product-list"),
+    path("waterless", views.waterless, name="waterless"),
+    path("orders", views.orders, name="orders"),
+    path("orderdetail/<int:id>", views.ordersdetail, name="order-detail"),
+    path("checkout", views.checkout, name="checkout"),
+    path("productdetail/<int:id>", views.productdetail, name="product-detail"),
+    path("cart/<int:id_>", views.cartView.as_view(), name="cart-functions"),
 ]
